2a0f38f04c2a93b9534a4ccb78f9aaa33ae00069
Lyrics <yhoonshine@gmail.com>
2019-07-07 11:39:43 +0800

    Init ASFeedList

---
+++ ASFeedList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASFeedList
+//
+//  Created by Lyrics on 2019/7/7.
+//  Copyright © 2019 Albatross Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
